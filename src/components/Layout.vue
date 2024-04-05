1204c53004187b58031dd5a31b395eab4e19f780
Michał Bojdo <87088719+michalbojdo@users.noreply.github.com>
2024-04-05 23:48:38 +0200

    Basic dark mode

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { ref, computed } from "vue"
+import { ref, computed, watch, onBeforeMount } from "vue"
 import { useDark, useTitle } from '@vueuse/core' 
-import { useRoute } from "vue-router";
+import { useRoute } from "vue-router"; 
 
 const currentRoute = computed(()=>'SUElections - '+ route.name?.toString())
 
@@ -9,7 +9,22 @@ const route = useRoute()
 const title = useTitle(currentRoute)
 const isDark = useDark()
 
+const textPrimary = ref()
+const textSecondary = ref()
+const bgPrimary = ref()
+const bgSecondary = ref()
+
 const drawer = ref(false)
+
+const setColorPalette = () =>{
+  [textPrimary.value, textSecondary.value, bgPrimary.value, bgSecondary.value] =
+  isDark.value ? 
+  ['text-grey-1','text-grey-5','bg-dark','bg-grey-9'] :
+  ['text-grey-1','text-grey-5','bg-primary','bg-grey-2']
+}
+
+onBeforeMount(setColorPalette)
+watch(isDark, setColorPalette)
 
 const menuList = [
   {
@@ -22,30 +37,35 @@ const menuList = [
     label: "Current polls",
     to: "/polls",
   },
+  {
+    icon: "group",
+    label: "Voters registry",
+    to: "/voters"
+  }
 ]
 </script>
 
 <template>
   <q-layout>
-    <q-header class="q-pa-lg">
+    <q-header :class="bgPrimary">
       <q-toolbar>
-        <q-toolbar-title> SUElections </q-toolbar-title>
-        <q-btn flat @click="drawer = !drawer" round dense icon="menu"></q-btn>
+        <q-toolbar-title :class="textPrimary+' q-pa-lg'"> SUElections </q-toolbar-title>
+        <q-btn flat @click="drawer = !drawer" round dense icon="menu" :class="textPrimary"></q-btn>
       </q-toolbar>
     </q-header>
     <q-drawer
       v-model="drawer"
       :width="200"
       :breakpoint="500"
+      :class="bgPrimary+' '+textSecondary"
       side="right"
       overlay
       bordered
-      :class="isDark ? 'bg-teal-1' : 'bg-teal-3'"
     >
       <q-scroll-area class="fit">
         <q-list>
           <template v-for="menuItem in menuList">
-            <q-item clickable :to="menuItem.to">
+            <q-item clickable :to="menuItem.to" :active-class="textPrimary">
               <q-item-section avatar>
                 <q-icon :name="menuItem.icon" />
               </q-item-section>
@@ -58,7 +78,7 @@ const menuList = [
       </q-scroll-area>
     </q-drawer>
     <q-page-container>
-      <q-page padding>
+      <q-page padding :class="bgSecondary+' '+textPrimary">
         <slot />
       </q-page>
     </q-page-container>
